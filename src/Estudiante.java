edc70b08a9c28d35075dea1f2e9ca1b9279772a2
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2020-01-03 20:24:03 -0600

    Nueva clase Estudiante

---
+++ src/Estudiante.java
@@ -0,0 +1,11 @@
+public class Estudiante {
+    String nombre = "";
+    String apellido = "";
+    String cedula = "";
+
+    public Estudiante(String nombre,String apellido, String cedula){
+        this.nombre = "Me llamo "+ nombre;
+        this.nombre = apellido;
+        this.nombre = " con "+ cedula;
+    }
+}
