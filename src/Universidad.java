927a7ff5a7a8ca2c9fb52ee475e543c0790afcd5
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2020-01-03 20:24:26 -0600

    Nueva clase Universidad

---
+++ src/Universidad.java
@@ -0,0 +1,11 @@
+public class Universidad {
+    String nombre = "";
+    String lema = "";
+    Integer telefono = 0;
+
+    public Universidad(){
+        nombre = "UML";
+        lema = "Ministerio Asambleas de Dios";
+        telefono = 12345678;
+    }
+}
