b3ff4020c3c82bc545ef80578bdc4c6278b54d6e
CPEREZNI <descarlos2013@gmail.com>
2020-01-03 21:10:18 -0600

    Cambio de titulo

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Universidad universidad = new Universidad();
         Estudiante estudiante = new Estudiante("Carlos Andres","Perez","123456789A");
         // create a jframe
-        JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+        JFrame frame = new JFrame("Ejemplo");
         JOptionPane.showMessageDialog(frame, estudiante.getInformacion(),"Hola",JOptionPane.PLAIN_MESSAGE);
         System.exit(0);
         //System.out.println(universidad.lema);
